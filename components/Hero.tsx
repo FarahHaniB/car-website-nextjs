55c3f11276a6aabe37f605999c0658dcb87b2068
FarahHaniB <109828677+FarahHaniB@users.noreply.github.com>
2024-02-29 17:06:06 +0800

    Added search bar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  const handleScroll = () => {}
+  const handleScroll = () => {};
 
   return (
     <div className="hero">
@@ -24,9 +24,10 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
-          <div className="hero__image-overlay"></div>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   );
